e8658f06636d434b1fa3075e600375a169b13ba2
Kathula-Rajkumar <rajkumar@Rajkumars-MacBook-Air.local>
2025-07-06 14:16:27 +0530

    implemented login & register with bcrypt and crypto token

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,6 +6,9 @@ import {Server } from "socket.io";
 import mongoose from "mongoose";
 
 import cors from "cors";
+import userRoutes from "./routes/users.routes.js";
+
+
 
 import connectToSocket from "./controllers/socketManager.js";
 
@@ -17,15 +20,16 @@ app.set("port", (process.env.PORT || 8000));
 app.use(cors());
 app.use(express.json({limit: "40kb"}));
 app.use(express.urlencoded({limit: "40kb", extended: true}));
+app.use("/api/v1/user", userRoutes);
+
 
 const start = async () => {
     app.set("mongo_user")
     const connectionDb = await mongoose.connect("mongodb+srv://rajkumarkathula15:rajkumar@zoomcluster.v6lgjzb.mongodb.net/");
     console.log(`MONGO connected DB Host: ${connectionDb.connection.host}`)
-    server.listen(app.get("port"),  ()=>{
-         console.log("listening on 8000");
-    });
-
+    server.listen(app.get("port"), () => {
+        console.log("LISTENIN ON PORT 8000")
+    });        
 
 }
 start();
